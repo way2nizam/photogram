a2f12da0f171126c64b90f49e11af817d6811563
nizamy710 <nizamy710@gamil.com>
2021-04-24 22:40:53 +0530

    fix: minor issue fixed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -14,9 +14,9 @@ const Modal = ({ setSelectedImg, selectedImg }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
-      <motion.img src={selectedImg} alt="enlarged pic" 
-        initial={{ y: "-100vh" }}
-        animate={{ y: 0 }}
+      <motion.img src={selectedImg} alt="pop out image" 
+        initial={{ x: "-100vh" }}
+        animate={{ x: 0 }}
       />
     </motion.div>
   )
