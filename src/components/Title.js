7b8437673daa55b85af703f1148eb80d00de9eba
nizamy710 <nizamy710@gamil.com>
2021-04-24 22:24:27 +0530

    feat: photo-portfolio
    photogram - photo portfolio using reactjs, useState, useEffect and firebase

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div className="title">
+        <h1>PhotoGram</h1>
+        <h2>Photo Portfolio</h2>
+        <p>A grid based photo gallery web site created using create-react-app and google firebase.</p>
+        </div>
+    )
+}
+
+export default Title;
---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+// import * as firebase from 'firebase/app';
+import firebase from 'firebase/app'
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBGefe5OwjSMGTP8ok6uVuj3FfmYzkDW1k",
+    authDomain: "nizam-firegram.firebaseapp.com",
+    projectId: "nizam-firegram",
+    storageBucket: "nizam-firegram.appspot.com",
+    messagingSenderId: "739198094870",
+    appId: "1:739198094870:web:b597ee898a5acdbbc26f37"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+  export { projectStorage, projectFirestore, timestamp };
