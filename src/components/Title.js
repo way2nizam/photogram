b58a9c24b0cd2d4721c08a2c922c076c54cb960d
nizamy710 <nizamy710@gamil.com>
2021-04-25 00:09:05 +0530

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
         <div className="title">
         <h1>PhotoGram</h1>
         <h2>Photo Portfolio</h2>
-        <p>A grid based photo gallery web site created using create-react-app, google firebase and framer motion library.</p>
+        <p>A grid-based photo gallery web app created using create-react-app, google firebase, and framer motion library.</p>
         </div>
     )
 }
